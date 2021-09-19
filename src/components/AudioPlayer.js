63549b0aee396cce2a5d3a231d9068614ee22f4e
Petrov Ilya <dnamix1@gmail.com>
2021-09-19 10:21:32 +0300

    setting up project boilerplate

---
+++ src/components/AudioPlayer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AudioPlayer.css';
+
+export default function AudioPlayer() {
+  return (
+    <React.Fragment>
+      <h1>AudioPlayer</h1>
+    </React.Fragment>
+  );
+}
