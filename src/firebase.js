31a74351c2cb94ff3b2c7528064db774c031b974
Petrov Ilya <dnamix1@gmail.com>
2021-09-19 10:45:04 +0300

    added firebase firestore, auth, google auth, storage buckets

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmb1Uw49c8-sBq4sQXYRDhtQLZvcE3bWI',
+  authDomain: 'whatsapp-clone-aa50e.firebaseapp.com',
+  projectId: 'whatsapp-clone-aa50e',
+  storageBucket: 'whatsapp-clone-aa50e.appspot.com',
+  messagingSenderId: '361401889826',
+  appId: '1:361401889826:web:c1fce133ebabb1d21c067b',
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const database = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const imagesStorage = firebase.storage().ref('images');
+const audioStorage = firebase.storage().ref('audios');
+const createTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+const serverTimestamp = firebase.database.ServerValue.TIMESTAMP;
+
+export {
+  database,
+  auth,
+  googleAuthProvider,
+  imagesStorage,
+  audioStorage,
+  createTimestamp,
+  serverTimestamp,
+};
