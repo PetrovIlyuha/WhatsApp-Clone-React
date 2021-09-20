31ec750bafae92f1aafa8b774256059e22ebc494
Petrov Ilya <dnamix1@gmail.com>
2021-09-20 14:24:36 +0300

    added image upload preview, messenger input with recording api integration

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,84 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useState } from 'react';
+import { useHistory, useParams } from 'react-router';
 import useRoom from '../hooks/useRoom';
+import ChatMessages from '../components/ChatMessages';
+import ChatFooter from '../components/ChatFooter';
+import MediaPreview from '../components/MediaPreview';
+import { Avatar, IconButton, Menu, MenuItem } from '@material-ui/core';
 import './Chat.css';
+import { AddPhotoAlternate, ArrowBack, MoreVert } from '@material-ui/icons';
 
 export default function Chat({ user, page }) {
   const { roomId } = useParams();
+  const history = useHistory();
+  const [image, setImage] = useState(null);
+  const [src, setSrc] = useState('');
   const room = useRoom(roomId, user.uid);
   console.log({ room });
-  return <div className='chat'>Chat</div>;
+
+  const showPreview = event => {
+    const file = event.target.files[0];
+    if (file) {
+      setImage(image);
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setSrc(reader.result);
+      };
+    }
+  };
+
+  const closePreview = () => {
+    setSrc('');
+    setImage(null);
+  };
+
+  return (
+    <div className='chat'>
+      <div style={{ height: page.height }} className='chat__background' />
+      <div className='chat__header'>
+        {page.isMobile && (
+          <IconButton onClick={history.goBack}>
+            <ArrowBack />
+          </IconButton>
+        )}
+        <div className='avatar__container'>
+          <Avatar src={room?.photoURL} />
+        </div>
+
+        <div className='chat__header--info'>
+          <h3 style={{ width: page.isMobile && page.width - 165 }}>
+            {room?.name}
+          </h3>
+        </div>
+        <div className='chat__header--right'>
+          <input
+            id='image'
+            style={{ display: 'none' }}
+            accept='image/*'
+            type='file'
+            onChange={showPreview}
+          />
+          <IconButton>
+            <label htmlFor='image' style={{ cursor: 'pointer', height: 24 }}>
+              <AddPhotoAlternate />
+            </label>
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+          <Menu id='menu' keepMounted open={false}>
+            <MenuItem>Delete Room</MenuItem>
+          </Menu>
+        </div>
+      </div>
+      <div className='chat__body--container'>
+        <div className='chat__body' style={{ height: page.height - 68 }}>
+          <ChatMessages />
+        </div>
+      </div>
+      <MediaPreview src={src} closePreview={closePreview} />
+      <ChatFooter />
+    </div>
+  );
 }
--- src/components/ChatFooter.js
+++ src/components/ChatFooter.js
@@ -1,6 +1,54 @@
-import React from "react";
-import "./ChatFooter.css";
+import React, { useState } from 'react';
+import {
+  CancelRounded,
+  CheckCircleRounded,
+  MicRounded,
+  Send,
+} from '@material-ui/icons';
+import './ChatFooter.css';
 
 export default function ChatFooter() {
-  return <div className="chat__footer">ChatFooter</div>;
+  const [isRecording, setIsRecording] = useState(false);
+
+  const buttonIcons = (
+    <>
+      <Send style={{ width: 20, height: 20, color: 'white' }} />
+      <MicRounded style={{ width: 24, height: 24, color: 'white' }} />
+    </>
+  );
+
+  const canRecord = navigator.mediaDevices.getUserMedia && window.MediaRecorder;
+
+  return (
+    <div className='chat__footer'>
+      <form>
+        <input type='text' placeholder='Type a message' />
+        {canRecord ? (
+          <button type='submit' className='send__btn'>
+            {buttonIcons}
+          </button>
+        ) : (
+          <>
+            <label htmlFor='capture' className='send__btn'>
+              {buttonIcons}
+              <input type='file' id='capture' accept='audio/*' capture />
+            </label>
+          </>
+        )}
+      </form>
+
+      {isRecording && (
+        <div className='record'>
+          <CancelRounded style={{ width: 30, height: 30, color: '#f20519' }} />
+          <div>
+            <div className='record__redcircle' />
+            <div className='record__duration' />
+          </div>
+          <CheckCircleRounded
+            style={{ width: 30, height: 30, color: '#41bf49' }}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
